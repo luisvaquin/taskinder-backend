e9028011c9285a63d69302256bf41928f2b1186a
Luis Vaquin <luisrodrigo2330@gmail.com>
2025-02-24 11:52:58 -0600

    update the vercel

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import app from "./src/app.js";
-import { connectDB } from "./src/db.js"
+import { connectDB } from "./src/db.js";
 
-connectDB()
-app.listen(5000)
-console.log('server on port runing', 5000)
+connectDB();
+app.listen(5000);
+console.log("server on port runing", 5000);
