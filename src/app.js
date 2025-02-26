1a7daf1ed9b7fce62f0924eaa812780476bb9ac5
Luis Vaquin <luisrodrigo2330@gmail.com>
2025-02-26 11:44:48 -0600

    update access the config a cors

--- src/app.js
+++ src/app.js
@@ -1,24 +1,33 @@
-import express from 'express'
-import morgan from 'morgan';
-import cookiePareser from 'cookie-parser'
-import cors from 'cors'
+import express from "express";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-import authRoutes from './routes/auth.routes.js'
-import taskRoutes from './routes/task.routes.js'
+import authRoutes from "./routes/auth.routes.js";
+import taskRoutes from "./routes/task.routes.js";
 
 const app = express();
 
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true
-}));
-app.use(morgan('dev'));
-app.use(express.json())
-app.use(cookiePareser());
+const allowedOrigins = ["http://localhost:5173", "https://taskinn.netlify.app"];
 
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
-app.use("/api", taskRoutes)
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api", taskRoutes);
 app.use("/api", authRoutes);
-
 
 export default app;
--- src/db.js
+++ src/db.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    try {
-        await mongoose.connect('mongodb://localhost/taskinderdb');
-        console.log('Connected to MongoDB');
-    } catch (e) {
-        console.error(e);
-    }
-}
+  try {
+    await mongoose.connect(
+      "mongodb+srv://luisrodrigo2330:admin@clustertaskinn.1zdic.mongodb.net/db_Taskinn"
+    );
+    console.log("Connected to MongoDB");
+  } catch (e) {
+    console.error(e);
+  }
+};
