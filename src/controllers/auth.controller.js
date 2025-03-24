20b456764f1d98a9f9f5c80015509a1fe610d4fc
Luis Vaquin <luisrodrigo2330@gmail.com>
2025-03-24 12:43:16 -0600

    the correction for backend

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -113,3 +113,20 @@ export const verifyToken = async (req, res) => {
     return res.status(500).json({ message: "Error al verificar el token" });
   }
 };
+
+export const profile = async (req, res) => {
+  const userFound = await User.findById(req.user.id);
+
+  if (!userFound)
+    return res.status(400).json({
+      message: "usuario no encontrado",
+    });
+
+  return res.json({
+    id: userFound._id,
+    username: userFound.username,
+    email: userFound.email,
+    createdAt: userFound.createdAt,
+    updatedAt: userFound.updatedAt,
+  });
+};
